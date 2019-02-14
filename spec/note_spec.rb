f3f0f550931974538ffcf324d642f99d4fa6b3bb
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-14 10:04:59 +0000

    The rspec file for noteformatter is amended.

--- spec/note_spec.rb
+++ spec/note_spec.rb
@@ -4,6 +4,7 @@ describe Note do
   let(:dbl){double "abc"}
   let(:subject){described_class.new("a","b",dbl)}
   it "tests the display method" do
-    allow(dbl).to receive(:format).with(subject)
+    expect(dbl).to receive(:format).with(subject)
+    subject.display()
   end
 end
