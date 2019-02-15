7884e9e5f938c4f754d088d0a552df27ba7a4267
Soroush Khosravi <sorush.kh68@gmail.com>
2019-02-15 10:44:59 +0000

    A change on the greet file and its testing file.

--- spec/greet_spec.rb
+++ spec/greet_spec.rb
@@ -2,10 +2,11 @@ require "greet"
 
 describe Greeter do
   let(:dbl2){double gets:dbl3 }
-  let(:dbl3){double chomp: "a"}
+  let(:dbl3){double chomp: dbl4}
+  let(:dbl4){double}
   let(:subject){described_class.new()}
   it "tsets the output" do
-    expect {subject.greet(dbl2)}.to output("What is your name?\nHello, a\n").to_stdout
+    expect {subject.greet(dbl2)}.to output("What is your name?\nHello, #{dbl4}\n").to_stdout
   end
 
 end
